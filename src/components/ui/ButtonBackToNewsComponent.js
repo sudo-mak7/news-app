7df34d2415b1348e0faca45de9976b095767431d
sudo-mak7 <66685649+sudo-mak7@users.noreply.github.com>
2022-11-29 21:30:41 +0400

    Revert "Update React Router to v6"

--- src/components/ui/ButtonBackToNewsComponent.js
+++ src/components/ui/ButtonBackToNewsComponent.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
-import { useNavigate  } from 'react-router-dom'
 
-const ButtonBackToNewsComponent = () => {
-
-  const navigate = useNavigate()
+const ButtonBackToNewsComponent = ({ props }) => {
   const clearCache = () => {
     localStorage.removeItem('currentNews')
     localStorage.removeItem('comments')
@@ -18,7 +15,7 @@ const ButtonBackToNewsComponent = () => {
       style={{ marginLeft: '1em', zIndex: '999', position: 'fixed' }}
       onClick={
         () => {
-          navigate(-1)
+          props.history.goBack()
           clearCache()
         }
       }
